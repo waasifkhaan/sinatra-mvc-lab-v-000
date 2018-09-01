ea8e65bf99a0c927caf3b5db88def97b610449e4
Wasif Khan <waasif.khaan@gmail.com>
2018-09-01 19:13:27 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end 
   
   post '/' do
-    @analyzed_text = .new(params[:user_text])
+    @latinized_text = PigLatinizer.new(params[:user_text])
 
     erb :results
   end

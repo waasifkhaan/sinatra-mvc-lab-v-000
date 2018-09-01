348fee3fd77dd9828e48393cee62716778cd2f39
Wasif Khan <waasif.khaan@gmail.com>
2018-09-01 19:22:02 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,8 +6,12 @@ class App < Sinatra::Base
   erb :user_input
   end 
   
-  post '/' do 
+  post '/' do
+    @latinized_text = PigLatinizer.new.piglatinize(params[:string])
+
+    erb :result
+  end
   
   
-  end 
+  
 end
